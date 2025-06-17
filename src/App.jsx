aedefcb97f5a4ff418da2275977a1e3a9540df74
Manichandana <reddymanichandana39@gmail.com>
2025-06-18 00:05:56 +0530

    fixed code-2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from './components/Home'
 import PageNotFound from './components/PageNotFound'
 import Jobs from './components/Jobs'
-import Login from './components/login'
+import Login from './components/Login'
 function App() {
   
 
